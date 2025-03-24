c07a4589a6353f9a5a2b970f294c997669716a9e
tharuka19981212 <kaveenrivindu@gmail.com>
2025-03-24 08:47:49 -0700

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,11 +81,11 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `T_H_A_R_U - M_D\n\nහායි\nසුබ දවසක්`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/tharuka19981212/BOT-HELPER/refs/heads/main/Add%20a%20heading.png",
               },
               caption: sid,
             });
